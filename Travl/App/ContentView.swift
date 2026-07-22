a7b808626aac50897254e0279fb26453421b555d
Vishal Bist <vishal.bist@iphtechnologies.com>
2026-07-22 18:53:24 +0530

    expense tab added

--- Travl/App/ContentView.swift
+++ Travl/App/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
 
                     NavigationStack {
 
-                        ExpenseDashboardView()
+                        ExpensesView()
                     }
                     .tabItem {
 
