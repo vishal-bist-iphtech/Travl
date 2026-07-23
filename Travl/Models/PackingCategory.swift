06f1083c1713b047d795668a8975647e95f09f26
Vishal Bist <vishal.bist@iphtechnologies.com>
2026-07-23 17:12:57 +0530

    Added packing category model

---
+++ Travl/Models/PackingCategory.swift
@@ -0,0 +1,18 @@
+//
+//  PackingCategory.swift
+//  Travl
+//
+//  Created by iPHTech 34 on 23/07/26.
+//
+
+import Foundation
+
+struct PackingCategory: Identifiable {
+
+    let id = UUID()
+
+    let name: String
+
+    let count: Int
+}
+
